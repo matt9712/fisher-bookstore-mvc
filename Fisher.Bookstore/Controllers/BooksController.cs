3c2540145c27a1624004147fd529bf5f52a38911
Matthew Herold <matt9712@gmail.com>
2019-02-06 20:49:39 -0500

    Commit

--- Fisher.Bookstore/Controllers/BooksController.cs
+++ Fisher.Bookstore/Controllers/BooksController.cs
@@ -6,14 +6,18 @@ namespace Fisher.Bookstore.Controllers
     [Route("books")]
     public class BooksController : Controller
     {
+        [Route("")]
+        [Route("index")]
         public IActionResult Index()
         {
             return Content("This is the books controller’s index action.");
         }
+        [Route("new")]
         public IActionResult New()
         {
             return Content("This is the books controller’s new action.");
         }
+        [Route("best-sellers")]
         public IActionResult Best()
         {
             return Content("This is the books controller’s best action.");
